75910123d938c2e71e5c835c0fcc377f5518acb5
EC2 Default User <ec2-user@ip-172-31-25-252.ap-northeast-1.compute.internal>
2022-11-09 15:44:42 +0000

    [update]users-mypage 退会フラッシュメッセージが出ないまま

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -22,13 +22,25 @@ class Public::UsersController < ApplicationController
   def confirm
   end
 
+  def withdraw
+    @user = current_user
+    # is_deletedカラムをtrueに変更することにより削除フラグを立てる
+    @user.update!(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行いたしました"
+    redirect_to root_path
+  end
+
+  def dummy
+    redirect_to new_user_registration_path
+  end
+
   private
   def update_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
   def ensure_correct_user
-
     unless current_user
       redirect_to users_my_page_path(current_user)
     end
