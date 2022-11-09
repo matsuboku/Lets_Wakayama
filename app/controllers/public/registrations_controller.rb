b401e3d428136c60c7348933d1df49337897e3cc
EC2 Default User <ec2-user@ip-172-31-25-252.ap-northeast-1.compute.internal>
2022-11-09 08:32:36 +0000

    [update]header,footer作成

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -52,7 +52,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
-  #   super(resource)
+  #   users_my_page_path
   # end
 
   # The path used after sign up for inactive accounts.
