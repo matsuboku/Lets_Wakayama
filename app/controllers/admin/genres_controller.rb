14dbe752ac51709f3e7f89a96775e1c70b599b70
EC2 Default User <ec2-user@ip-172-31-25-252.ap-northeast-1.compute.internal>
2022-11-10 07:07:04 +0000

    [update]genres edit画面作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -17,6 +17,18 @@ class Admin::GenresController < ApplicationController
   end
 
   def edit
+    @genre = Genre.find(params[:id])
+  end
+  
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+     flash[:notice] = "更新しました"
+     redirect_to admin_genres_path
+    else
+     @genre = Genre.find(params[:id])
+     render 'edit'
+    end
   end
   
   private
