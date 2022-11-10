fab87b03d7dfb3f87d9773a8383125769109c03a
EC2 Default User <ec2-user@ip-172-31-25-252.ap-northeast-1.compute.internal>
2022-11-10 08:43:16 +0000

    [update]admin-users edit画面作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,5 +8,21 @@ class Admin::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(update_params)
+       flash[:notice] = "更新しました。"
+       redirect_to admin_user_path(@user)
+    else
+       render :edit
+    end
+  end
+
+  private
+  def update_params
+    params.require(:user).permit(:name, :email, :is_deleted)
   end
 end
