99d88a8419ccd945ca9d6f5445147a8be2f4246d
EC2 Default User <ec2-user@ip-172-31-25-252.ap-northeast-1.compute.internal>
2022-11-13 11:20:27 +0000

    [update]posts edit,destroy作成

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -19,15 +19,32 @@ class Public::PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.delete
+    redirect_to posts_path
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post), notice: "更新しました."
+    else
+      render "edit"
+    end
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:image, :title, :explanation, :genre_id)
   end
-  
+
   def correct_user
-    @book = Book.find(params[:id])
-    @user = @book.user
-    redirect_to(books_path) unless @user == current_user
+    @post = Post.find(params[:id])
+    @user = @post.user
+    redirect_to(posts_path) unless @user == current_user
   end
 end
--- db/migrate/20221113023509_create_posts.rb
+++ db/migrate/20221113023509_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
     create_table :posts do |t|
       t.string :title, null: false
       t.text :explanation, null: false
-      t.integer :user_id, null: false
+      t.integer :user_id
       t.integer :genre_id, null: false
       t.timestamps
     end
