7f4ece142e1b4a56613ec1fea47831f9c1046768
EC2 Default User <ec2-user@ip-172-31-25-252.ap-northeast-1.compute.internal>
2022-11-13 02:49:28 +0000

    [update]post_model作成

---
+++ db/migrate/20221113023509_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.string :title, null: false
+      t.text :explanation, null: false
+      t.integer :user_id, null: false
+      t.integer :genre_id, null: false
+      t.string :address
+      t.float :latitude
+      t.float :longitude
+      t.timestamps
+    end
+  end
+end
