3a896bd99c3bf4a3b5faf4cd4880e320e67ee5e5
EC2 Default User <ec2-user@ip-172-31-25-252.ap-northeast-1.compute.internal>
2022-11-13 05:19:57 +0000

    [update]posts new画面作成

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,10 +1,24 @@
 class Public::PostsController < ApplicationController
-  def new,
+  def new
+    @post = Post.new
   end
 
-  def index,
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to posts_path
+  end
+
+  def index
   end
 
   def show
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :title, :explanation)
   end
 end
