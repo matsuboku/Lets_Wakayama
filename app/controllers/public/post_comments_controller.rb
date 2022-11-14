9c39764da091cf9ef3e14f9f9cd68894db49a198
EC2 Default User <ec2-user@ip-172-31-25-252.ap-northeast-1.compute.internal>
2022-11-14 04:56:03 +0000

    [update]users_routing修正

---
+++ app/controllers/public/post_comments_controller.rb
@@ -0,0 +1,21 @@
+class Public::PostCommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    comment = current_user.post_comments.new(post_comment_params)
+    comment.post_id = @post.id
+    comment.save
+    @post_comment = PostComment.new
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @post_comment = PostComment.find_by(id: params[:id], post_id: params[:post_id])
+    @post_comment.destroy
+    @post_comment = PostComment.new
+  end
+
+  private
+  def post_comment_params
+    params.require(:post_comment).permit(:comment)
+  end
+end
