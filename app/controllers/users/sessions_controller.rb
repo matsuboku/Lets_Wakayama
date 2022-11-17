2e661d8d75621cd84f89a2093e1fb0d8ed39f277
EC2 Default User <ec2-user@ip-172-31-25-252.ap-northeast-1.compute.internal>
2022-11-17 13:07:23 +0000

    [finish]map表示完成

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,6 @@ class Users::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to users_my_page_path(user), notice: 'guestuserでログインしました。'
+    redirect_to users_path(user), notice: 'guestuserでログインしました。'
   end
 end
--- app/models/map.rb
+++
@@ -1,6 +0,0 @@
-class Map < ApplicationRecord
-  belongs_to :post
-
-  validates :latitude, presence: true
-  validates :longitude, presence: true
-end
