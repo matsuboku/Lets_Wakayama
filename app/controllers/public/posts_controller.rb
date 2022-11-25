c2ff272f70ea5c76f66bc5de68af5a7319e87ff0
EC2 Default User <ec2-user@ip-172-31-25-252.ap-northeast-1.compute.internal>
2022-11-25 10:41:25 +0000

    [add]アクセス制限

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,5 +1,6 @@
 class Public::PostsController < ApplicationController
   before_action :authenticate_user!, only:[:new, :create]
+  
   before_action :correct_user, only:[:edit, :update]
 
   def new
